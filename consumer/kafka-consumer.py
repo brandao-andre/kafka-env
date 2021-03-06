5c4d3b08fac9fbb59f160d3bd522c4b6838e3a34
André Brandão <andrebrandao@i4sea.com>
2021-03-06 18:47:46 -0300

    :tada: kafka environment with simple python producer/consumer

---
+++ consumer/kafka-consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+
+# continuous loop
+var = 1
+while var == 1:
+
+    # initialize consumer to given topic and broker
+    consumer = KafkaConsumer('test-topic',
+                            group_id='consumer-1',
+                            bootstrap_servers='localhost:9092')
+
+    # loop and print messages
+    for msg in consumer:
+        print (msg)
+
